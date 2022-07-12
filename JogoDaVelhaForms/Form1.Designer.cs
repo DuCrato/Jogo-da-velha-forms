406769f39fdaaaa987b6f72e86cd32449dda382a
DuCrato <williamanderson1994@hotmail.com>
2022-07-12 16:21:02 -0300

    first commit

---
+++ JogoDaVelhaForms/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace JogoDaVelhaForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbl_tie = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_o_score = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_x_score = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_resete = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_8);
+            this.panel1.Controls.Add(this.btn_7);
+            this.panel1.Controls.Add(this.btn_6);
+            this.panel1.Controls.Add(this.btn_5);
+            this.panel1.Controls.Add(this.btn_4);
+            this.panel1.Controls.Add(this.btn_3);
+            this.panel1.Controls.Add(this.btn_2);
+            this.panel1.Controls.Add(this.btn_1);
+            this.panel1.Controls.Add(this.btn_0);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(338, 340);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_8
+            // 
+            this.btn_8.BackColor = System.Drawing.Color.White;
+            this.btn_8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_8.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_8.Location = new System.Drawing.Point(224, 225);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(100, 100);
+            this.btn_8.TabIndex = 8;
+            this.btn_8.UseVisualStyleBackColor = false;
+            this.btn_8.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.BackColor = System.Drawing.Color.White;
+            this.btn_7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_7.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_7.Location = new System.Drawing.Point(118, 225);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(100, 100);
+            this.btn_7.TabIndex = 7;
+            this.btn_7.UseVisualStyleBackColor = false;
+            this.btn_7.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.BackColor = System.Drawing.Color.White;
+            this.btn_6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_6.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_6.Location = new System.Drawing.Point(12, 225);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(100, 100);
+            this.btn_6.TabIndex = 6;
+            this.btn_6.UseVisualStyleBackColor = false;
+            this.btn_6.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.BackColor = System.Drawing.Color.White;
+            this.btn_5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_5.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_5.Location = new System.Drawing.Point(224, 119);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(100, 100);
+            this.btn_5.TabIndex = 5;
+            this.btn_5.UseVisualStyleBackColor = false;
+            this.btn_5.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.BackColor = System.Drawing.Color.White;
+            this.btn_4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_4.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_4.Location = new System.Drawing.Point(118, 119);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(100, 100);
+            this.btn_4.TabIndex = 4;
+            this.btn_4.UseVisualStyleBackColor = false;
+            this.btn_4.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.BackColor = System.Drawing.Color.White;
+            this.btn_3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_3.Location = new System.Drawing.Point(12, 119);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(100, 100);
+            this.btn_3.TabIndex = 3;
+            this.btn_3.UseVisualStyleBackColor = false;
+            this.btn_3.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.BackColor = System.Drawing.Color.White;
+            this.btn_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_2.Location = new System.Drawing.Point(224, 13);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(100, 100);
+            this.btn_2.TabIndex = 2;
+            this.btn_2.UseVisualStyleBackColor = false;
+            this.btn_2.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.BackColor = System.Drawing.Color.White;
+            this.btn_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_1.Location = new System.Drawing.Point(118, 13);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(100, 100);
+            this.btn_1.TabIndex = 1;
+            this.btn_1.UseVisualStyleBackColor = false;
+            this.btn_1.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_0
+            // 
+            this.btn_0.BackColor = System.Drawing.Color.White;
+            this.btn_0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_0.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_0.Location = new System.Drawing.Point(12, 13);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(100, 100);
+            this.btn_0.TabIndex = 0;
+            this.btn_0.UseVisualStyleBackColor = false;
+            this.btn_0.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lbl_tie);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.lbl_o_score);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.lbl_x_score);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(356, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(314, 137);
+            this.panel2.TabIndex = 1;
+            // 
+            // lbl_tie
+            // 
+            this.lbl_tie.AutoSize = true;
+            this.lbl_tie.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_tie.ForeColor = System.Drawing.Color.White;
+            this.lbl_tie.Location = new System.Drawing.Point(205, 89);
+            this.lbl_tie.Name = "lbl_tie";
+            this.lbl_tie.Size = new System.Drawing.Size(28, 32);
+            this.lbl_tie.TabIndex = 5;
+            this.lbl_tie.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(78, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Empate:";
+            // 
+            // lbl_o_score
+            // 
+            this.lbl_o_score.AutoSize = true;
+            this.lbl_o_score.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_o_score.ForeColor = System.Drawing.Color.White;
+            this.lbl_o_score.Location = new System.Drawing.Point(205, 57);
+            this.lbl_o_score.Name = "lbl_o_score";
+            this.lbl_o_score.Size = new System.Drawing.Size(28, 32);
+            this.lbl_o_score.TabIndex = 3;
+            this.lbl_o_score.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(78, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 32);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "O Vitoria:";
+            // 
+            // lbl_x_score
+            // 
+            this.lbl_x_score.AutoSize = true;
+            this.lbl_x_score.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_x_score.ForeColor = System.Drawing.Color.White;
+            this.lbl_x_score.Location = new System.Drawing.Point(205, 25);
+            this.lbl_x_score.Name = "lbl_x_score";
+            this.lbl_x_score.Size = new System.Drawing.Size(28, 32);
+            this.lbl_x_score.TabIndex = 1;
+            this.lbl_x_score.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(78, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X Vitoria:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btn_resete);
+            this.panel3.Location = new System.Drawing.Point(356, 227);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(314, 125);
+            this.panel3.TabIndex = 6;
+            // 
+            // btn_resete
+            // 
+            this.btn_resete.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_resete.Location = new System.Drawing.Point(11, 36);
+            this.btn_resete.Name = "btn_resete";
+            this.btn_resete.Size = new System.Drawing.Size(286, 51);
+            this.btn_resete.TabIndex = 0;
+            this.btn_resete.Text = "Resetar";
+            this.btn_resete.UseVisualStyleBackColor = true;
+            this.btn_resete.Click += new System.EventHandler(this.btn_resete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(682, 366);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbl_tie;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_o_score;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_x_score;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btn_resete;
+    }
+}
+
---
+++ JogoDaVelhaForms/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelhaForms
+{
+    public partial class Form1 : Form
+    {
+        private int xScore = 0,
+            oScore = 0,
+            tie    = 0,
+            round  = 0;
+
+        private bool shift = true,
+            winGame = false;
+
+        private string[] listGame = new string[9];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_resete_Click(object sender, EventArgs e)
+        {
+            btn_0.Text = "";
+            btn_1.Text = "";
+            btn_2.Text = "";
+            btn_3.Text = "";
+            btn_4.Text = "";
+            btn_5.Text = "";
+            btn_6.Text = "";
+            btn_7.Text = "";
+            btn_8.Text = "";
+
+            round = 0;
+            winGame = false;
+           
+            for (int i = 0; i < 9; i++)
+            {
+                listGame[i] = "";
+            }
+        }
+
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            int buttonIndex = btn.TabIndex;
+
+            if (btn.Text == "" && winGame == false)
+            {
+                if (shift)
+                {
+                    btn.Text = "X";
+                    listGame[buttonIndex] = btn.Text;
+                    round++;
+                    shift = !shift;
+                    CheckPlayer(1);
+                }
+                else
+                {
+                    btn.Text = "O";
+                    listGame[buttonIndex] = btn.Text;
+                    round++;
+                    shift = !shift;
+                    CheckPlayer(2);
+                }
+            }
+        }
+
+        void CheckPlayer(int player)
+        {
+            string aux;
+
+            if (player == 1)
+            {
+                aux = "X";
+            }
+            else
+            {
+                aux = "0";
+            }
+
+            for (int horizotal = 0; horizotal < 8; horizotal += 3)
+            {
+                if (aux == listGame[horizotal])
+                {
+                    if (listGame[horizotal] == listGame[horizotal + 1] && listGame[horizotal] == listGame[horizotal + 2])
+                    {
+                        MessageBox.Show("Teste Horizotal");
+                        return;
+                    }
+                }
+            }
+
+            for (int vertical = 0; vertical < 3; vertical++)
+            {
+                if (aux == listGame[vertical])
+                {
+                    if (listGame[vertical] == listGame[vertical + 3] && listGame[vertical] == listGame[vertical + 6])
+                    {
+                        MessageBox.Show("Teste Vertical");
+                        return;
+                    }
+                }
+            }
+
+            if (listGame[0] == aux)
+            {
+                if (listGame[0] == listGame[4] && listGame[0] == listGame[8])
+                {
+                    MessageBox.Show("Teste Dionanal esquerda direita");
+                    return;
+                }
+            }
+
+            if (listGame[2] == aux)
+            {
+                if (listGame[2] == listGame[4] && listGame[2] == listGame[6])
+                {
+                    MessageBox.Show("Teste Diagonal direita esquerda");
+                    return;
+                }
+            }
+
+            if (round == 9 && winGame == false)
+            {
+                tie++;
+                lbl_tie.Text = Convert.ToString(tie);
+                MessageBox.Show("Empate!!!");
+                winGame = true;
+                return;
+            }
+        }
+
+        void WinPlayer(int player)
+        {
+            winGame = true;
+
+            if (player == 1)
+            {
+                xScore++;
+                lbl_x_score.Text = Convert.ToString(xScore);
+                MessageBox.Show("Jogador X ganhou!!!");
+                shift = true;
+            }
+            else
+            {
+                oScore++;
+                lbl_o_score.Text = Convert.ToString(oScore);
+                MessageBox.Show("Jogador O ganhou!!!");
+                shift = true;
+            }
+        }
+    }
+}
